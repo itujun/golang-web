3481c6f25eddac41e01769f4b45d36d5f31abe54
Juna <itujun7@gmail.com>
2025-07-25 01:53:11 +0700

    Header: Request Header

--- query_param_test.go
+++ query_param_test.go
@@ -68,3 +68,21 @@ func TestMultipleValueQueryParameter(t *testing.T) {
 	
 	fmt.Println(string(body))
 }
+
+
+func RequestHeader(w http.ResponseWriter, r *http.Request){
+	contentType := r.Header.Get("Content-Type")
+	fmt.Fprint(w, contentType)
+}
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	request.Header.Add("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	
+	fmt.Println(string(body))
+}
