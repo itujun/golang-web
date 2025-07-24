81195a697ca4e47c27e185f7ed4c3292ecd162ff
Juna <itujun7@gmail.com>
2025-07-25 05:49:35 +0700

    Template Data: Struct

--- template_data_test.go
+++ template_data_test.go
@@ -14,6 +14,9 @@ func TemplateDataMap(writer http.ResponseWriter, request *http.Request) {
 	tmpl.ExecuteTemplate(writer, "name.gohtml", map[string]interface{}{
 		"Title": "Template Data Map",
 		"Name":  "Lev",
+		"Address": map[string]interface{}{
+			"Street": "Jl. in aja dulu",
+		},
 	})
 }
 
@@ -22,6 +25,38 @@ func TestTemplateDataMap(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	TemplateDataMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+
+type Address struct {
+	Street string
+}
+
+type Page struct {
+	Title string
+	Name  string
+	Address Address
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	tmpl.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Template Data Struct",
+		Name:  "Lev",
+		Address: Address{
+			Street: "Jl. in aja dulu",
+		},
+	})
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
 
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
