ed97409c7458520972b9ad24bc6959702136dd18
Juna <itujun7@gmail.com>
2025-07-25 06:22:13 +0700

    Template Action: Range

--- template_action_test.go
+++ template_action_test.go
@@ -55,3 +55,24 @@ func TestTemplateActionOperator(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+
+func TemplateActionRange(writer http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./templates/range.gohtml"))
+	tmpl.ExecuteTemplate(writer, "range.gohtml", map[string]interface{}{
+		"Title": "Template Action Range",
+		"Hobbies":  []string{
+			// "Coding", "Gaming", "Sleeping",
+		},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
