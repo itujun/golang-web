7b92546fb15477dc74eef8c99e72a5adfe09c51a
Juna <itujun7@gmail.com>
2025-07-25 22:52:55 +0700

    Middleware: contoh untuk Error Handler

--- middleware_test.go
+++ middleware_test.go
@@ -16,6 +16,21 @@ func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *
 	fmt.Println("Response Sent")
 }
 
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorHandler *ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Recovered from panic:", err)
+			http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+		}
+	}()
+
+	errorHandler.Handler.ServeHTTP(writer, request)
+}
+
 func TestMiddleware(t *testing.T){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
@@ -26,15 +41,29 @@ func TestMiddleware(t *testing.T){
 		fmt.Println("Foo Executed")
 		fmt.Fprint(writer, "Middleware Test Foo")
 	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Panic Executed")
+		panic("This is a panic")
+	})
 
 	logMiddleware := &LogMiddleware{
 		Handler: mux,
 	}
 
-	server := http.Server{
-		Addr:    "localhost:8080",
+	errorHandler := &ErrorHandler{
 		Handler: logMiddleware,
 	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: errorHandler,
+	}
+
+	// ? Kalau dirunut: 
+	// ? maka request akan masuk ke Server, 
+	// ? lalu ke ErrorHandler, 
+	// ? lalu ke LogMiddleware, 
+	// ? dan terakhir ke mux.
 
 	err := server.ListenAndServe()
 	if err != nil {
