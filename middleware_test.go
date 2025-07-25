828ac64755238c0fd3e0b6019f35ff9f1c0c21b4
Juna <itujun7@gmail.com>
2025-07-25 22:43:14 +0700

    Middleware

---
+++ middleware_test.go
@@ -0,0 +1,43 @@
+package golangweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Request Received:", request.Method, request.URL.Path)
+	middleware.Handler.ServeHTTP(writer, request)
+	fmt.Println("Response Sent")
+}
+
+func TestMiddleware(t *testing.T){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Executed")
+		fmt.Fprint(writer, "Middleware Test")
+	})
+	mux.HandleFunc("/foo", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Foo Executed")
+		fmt.Fprint(writer, "Middleware Test Foo")
+	})
+
+	logMiddleware := &LogMiddleware{
+		Handler: mux,
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: logMiddleware,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
